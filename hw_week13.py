af4f8fa4dc7b1d9208a0ef9d8c781f18c44e3c8b
skuldx <61177084+skuldx@users.noreply.github.com>
2020-04-08 11:35:31 +0800

    Update hw_week13.py

--- hw_week13.py
+++ hw_week13.py
@@ -1,7 +1,19 @@
 def hw_week13(number):
-    # Your code here
-    pass
+    #number = input("Please input an integer:")
 
+    try:
+        while int(number) != 1:
+            mod_result = int(number) % 2    
+            if mod_result == 0:
+                number = int(number) // 2
+                print(str(number) + " ", end = " ")
+       
+            elif mod_result == 1:
+                number = int(number) * 3 + 1
+                print(str(number) + " ", end = " ")
+   
+    except Exception as e:
+        print(e)
 
 if __name__ == '__main__':
     hw_week13(input('Please input the positive integer: '))
